c07081d77bbb66e5826345f6c0b0bd355db4e61a
Paul Chen <paulchen647@gmail.com>
2020-12-19 17:30:15 +0800

    Change circleci config

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,3 +4,8 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
 import "index.scss";
+
+import Navbar from '../components/Navbar';
+
+storiesOf("Navbar", module)
+.add("Base", ()=> <Navbar />)
